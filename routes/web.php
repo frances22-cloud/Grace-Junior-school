2dd2c2e73fb33d0c92402043de7194e7ac371ef3
Frances Ayango <frances.ayango20@gmail.com>
2026-01-10 20:00:03 +0300

    modified home route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('frontend.home');
+});
+
+Route::get('/', function () {
+    return redirect('/home');
 });
 
 Route::get('/about', function () {
